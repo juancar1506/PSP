f307f385c6276567210533e5cb9e6e3a6f6dde7f
Juan Carlos Tauroni <jctr2002@gmail.com>
2023-02-18 18:05:20 +0100

    EJ2

--- Threads/src/EJ2/ProductorConsumidor.java
+++ Threads/src/EJ2/ProductorConsumidor.java
@@ -2,11 +2,15 @@ package EJ2;
 
 public class ProductorConsumidor {
     public static void main(String[] args) {
+        // se crea el número
         Numero numero = new Numero();
+
+        // Se cre el consumidor
         Consumidor c1 = new Consumidor("Juan", numero);
         
         new Thread(c1).start();
 
+        // Se crea el productor 
         Productor p = new Productor(numero);
         new Thread(p).start();
 
